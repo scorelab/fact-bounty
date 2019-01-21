da139d4e434b29c8c0255c02fd90d2c6adbaf3de
Oshan Mudannayake <oshan.ivantha@gmail.com>
2019-01-21 10:54:01 +0530

    Add login backend functions
    - Mongoose
    - Passport
    - Routes
    - Models

---
+++ fact-bounty-server/server.js
@@ -0,0 +1,36 @@
+const express    = require('express')
+const mongoose   = require('mongoose')
+const bodyParser = require('body-parser')
+const passport   = require('passport')
+
+const users = require('./routes/api/user')
+
+const app = express()
+
+// Bodyparser middleware
+app.use(
+	bodyParser.urlencoded({
+		extended: false
+	})
+)
+app.use(bodyParser.json())
+
+// DB Config
+const db = require('./config/keys').mongoURI
+
+// Connect to MongoDB
+mongoose.connect(db, {useNewUrlParser: true})
+	.then(() => console.log('MongoDB successfully connected'))
+	.catch(err => console.log(err))
+
+// Passport middleware
+app.use(passport.initialize())
+
+// Passport config
+require('./config/passport')(passport)
+
+// Routes
+app.use('/api/users', users)
+
+const port = process.env.PORT || 4000
+app.listen(port, () => console.log(`Server up and running on port ${port} !`))
