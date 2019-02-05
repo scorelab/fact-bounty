c66e8c1d8adfaf8c66a1bc6e3787b6755f2c149b
Oshan Mudannayake <oshan.ivantha@gmail.com>
2019-02-05 10:23:50 +0530

    Implement the API routes for stories

--- fact-bounty-server/config/passport.js
+++ fact-bounty-server/config/passport.js
@@ -9,15 +9,9 @@ opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
 opts.secretOrKey    = keys.secretOrKey
 
 module.exports = passport => {
-	passport.use(
-		new JwtStrategy(opts, (jwt_payload, done) => {
+	passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
 			User.findById(jwt_payload.id)
-				.then(user => {
-					if (user) {
-						return done(null, user)
-					}
-					return done(null, false)
-				})
+				.then(user => user ? done(null, user) : done(null, false))
 				.catch(err => console.log(err))
 		})
 	)
