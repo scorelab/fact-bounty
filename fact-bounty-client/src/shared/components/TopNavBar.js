5af50432a3f881cb8aec6e7d996da663973f2a85
root5533 <ymuraad@gmail.com>
2019-02-06 18:45:08 +0530

    Add routers for home, about

--- fact-bounty-client/src/shared/components/TopNavBar.js
+++ fact-bounty-client/src/shared/components/TopNavBar.js
@@ -5,6 +5,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { Person, ExpandMore } from '@material-ui/icons';
 import { Menu, MenuItem } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 class TopNavBar extends Component {
 
@@ -17,7 +18,7 @@ class TopNavBar extends Component {
             backgroundColor: '#fafafa'
         },
         navbarTitle: {
-            flexGrow: 1,
+            flexGrow: 2,
             textAlign: 'center',
             fontVariant: 'small-caps',
             letterSpacing: '1px',
@@ -45,9 +46,14 @@ class TopNavBar extends Component {
         return (
             <AppBar position="static" color="default" style={this.styles.navbar}>
                 <Toolbar>
+                    <div style={{ height: '10px', width: '14%' }}></div>
                     <Typography variant="h3" color="primary" style={this.styles.navbarTitle}>Fact Bounty</Typography>
-                    <Button color="primary" style={this.styles.navbarLinks}>HOME</Button>
-                    <Button color="primary" style={this.styles.navbarLinks}>ABOUT</Button>
+                    <Link to="/" style={{ textDecoration: 'none' }}>
+                        <Button color="primary" style={this.styles.navbarLinks}>HOME</Button>
+                    </Link>
+                    <Link to="/about" style={{ textDecoration: 'none' }}>
+                        <Button color="primary" style={this.styles.navbarLinks} >ABOUT</Button>
+                    </Link>
                     <Button color="primary" style={this.styles.navbarLinks}
                         onClick={this.handleToggle} aria-haspopup="true"
                         aria-owns={anchorEl ? 'simple-menu' : undefined}>
