95069b162994454afa5b1dc715b9776be0a7d7de
root5533 <ymuraad@gmail.com>
2019-02-07 13:51:11 +0530

    Set lines ellipsis

--- fact-bounty-client/src/pages/main/components/Post.js
+++ fact-bounty-client/src/pages/main/components/Post.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Card, CardContent } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import postImg1 from '../president.jpg';
+import LinesEllipsis from 'react-lines-ellipsis';
 import '../Posts.sass';
 
 const styles = {
@@ -8,7 +11,6 @@ const styles = {
     },
     card: {
         marginBottom: '7px',
-        height: '175px',
         boxShadow: '0 0 0 0',
         backgroundColor: '#fafafa',
         color: '#424242'
@@ -21,27 +23,50 @@ class Post extends Component {
         super(props);
         this.state = {
             postStatus: 'true',
-            userInput: null
+            userInput: null,
+            postContent: ""
         }
+    }
+
+    componentDidMount() {
+        this.setState({
+            postContent: this.props.post.content
+        })
+    }
+
+    handleReflow = (rleState) => {
+        console.log("reflow");
     }
 
     render() {
         const { post } = this.props;
         return (
-            <Card style={styles.card}>
-                <CardContent style={styles.cardContent}>
-                    <div className="container">
-                        <div className="image">
-                            <img src="" alt="fact-bounty"></img>
+            <div className="hover-container">
+                <Card style={styles.card}>
+                    <CardContent style={styles.cardContent}>
+                        <div className="container">
+                            <div className="image">
+                                <img src={postImg1} alt="fact-bounty" className="post-img"></img>
+                            </div>
+                            <div className="details">
+                                {/* <div className="title"><span>{post.title}</span></div> */}
+                                <div className="title">{post.title}</div>
+                                <div className="date">{post.date}</div>
+                                <div className="content">
+                                    <LinesEllipsis
+                                        text={this.state.postContent}
+                                        maxLine='3'
+                                        ellipsis='...'
+                                        onReflow={this.handleReflow}
+                                    />
+                                </div>
+                                <div className="read-more"><Link to="/post">Read more</Link></div>
+                            </div>
                         </div>
-                        <div className="details">
-                            <div className="title"><h4>{post.title}</h4></div>
-                            <div className="content"><p>{post.content}</p></div>
-                        </div>
-                    </div>
-                    <div className="vote-status"></div>
-                </CardContent>
-            </Card>
+                        <div className="vote-status"></div>
+                    </CardContent>
+                </Card>
+            </div>
         )
     }
 
