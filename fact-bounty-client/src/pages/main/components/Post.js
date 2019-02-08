2fb6cfc6e673a455591b98e53503900d7a8b2093
root5533 <ymuraad@gmail.com>
2019-02-08 23:14:08 +0530

    Add vote buttons

--- fact-bounty-client/src/pages/main/components/Post.js
+++ fact-bounty-client/src/pages/main/components/Post.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import { Card, CardContent } from '@material-ui/core';
+import { Card, CardContent, Icon } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import postImg1 from '../flag1.jpg';
 import postImg2 from '../flag2.jpg';
 import postImg3 from '../president.jpg';
 import LinesEllipsis from 'react-lines-ellipsis';
+import { withStyles } from '@material-ui/core/styles';
 import '../Posts.sass';
+import ReportProblem from '@material-ui/icons/ReportProblemOutlined';
+import Cancel from '@material-ui/icons/CancelOutlined';
 
 const styles = {
     cardContent: {
@@ -13,9 +16,31 @@ const styles = {
     },
     card: {
         marginBottom: '7px',
-        boxShadow: '0 0 0 0',
+        // boxShadow: '0 0 0 0',
         backgroundColor: '#fafafa',
-        color: '#424242'
+        color: '#424242',
+        transition: '0.1s ease',
+        '&:hover': {
+            boxShadow: '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)',
+            '&:cardContent': {
+                padding: '20px'
+            }
+        }
+    },
+    icon: {
+        fontSize: '30px'
+    },
+    trueBtnHover: {
+        color: '#009688',
+        fontSize: '30px'
+    },
+    fakeBtnHover: {
+        color: '#f4511e',
+        fontSize: '30px'
+    },
+    mixBtnHover: {
+        color: '#0288d1',
+        fontSize: '30px'
     }
 }
 
@@ -36,32 +61,21 @@ class Post extends Component {
         })
     }
 
-    handleReflow = (rleState) => {
-        console.log("reflow");
-    }
-
     render() {
-        const { post } = this.props;
+        const { post, classes } = this.props;
         const content = post.content.slice(0, 320);
         return (
             <div className="hover-container">
-                <Card style={styles.card}>
+                <Card className={classes.card}>
                     <CardContent style={styles.cardContent}>
                         <div className="container">
                             <div className="image">
                                 <img src={postImg1} alt="fact-bounty" className="post-img"></img>
                             </div>
                             <div className="details">
-                                {/* <div className="title"><span>{post.title}</span></div> */}
                                 <div className="title">{post.title}</div>
                                 <div className="date">{post.date}</div>
                                 <div className="content">
-                                    {/* <LinesEllipsis
-                                        text={this.state.postContent}
-                                        maxLine='3'
-                                        ellipsis='...'
-                                        onReflow={this.handleReflow}
-                                    /> */}
                                     {content}
                                     {(post.content.length > 320) ? '...' : ''}
                                 </div>
@@ -71,10 +85,37 @@ class Post extends Component {
                         <div className="vote-status"></div>
                     </CardContent>
                 </Card>
+                <div className="btn-column">
+                    <div className="true-transition">
+                        <div className="true-btn"><Icon className={classes.icon}>check_circle_outline</Icon></div>
+                        <div className="true-btn-hover">
+                            <Icon className={classes.trueBtnHover}>check_circle</Icon>
+                            <div className="true-label">True</div>
+                        </div>
+                    </div>
+                    <div className="fake-transition">
+                        <div className="fake-btn"><Cancel className={classes.icon} /></div>
+                        <div className="fake-btn-hover">
+                            <Icon className={classes.fakeBtnHover}>cancel</Icon>
+                            <div className="fake-label">Fake</div>
+                        </div>
+                    </div>
+                    <div className="mix-transition">
+                        <div className="mix-btn"><ReportProblem className={classes.icon} /></div>
+                        <div className="mix-btn-hover">
+                            <Icon className={classes.mixBtnHover}>report_problem</Icon>
+                            <div className="mix-label">Mixture</div>
+                        </div>
+                    </div>
+                </div>
+                {/* <div className="btn-column-hover">
+                    <div className="fake-btn"><Cancel className={classes.icon} /></div>
+                    <div className="mix-btn"><ReportProblem className={classes.icon} /></div>
+                </div> */}
             </div>
         )
     }
 
 }
 
-export default Post;
+export default withStyles(styles)(Post);
