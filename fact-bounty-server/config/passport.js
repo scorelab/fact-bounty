83c993f536a2ededa6d04adc6fc2335a4723bd46
Tushar Varshney <juniorvarshney19@gmail.com>
2019-02-15 22:29:18 +0530

    Restructured and reformated project with eslint and standard js

--- fact-bounty-server/config/passport.js
+++ fact-bounty-server/config/passport.js
@@ -10,9 +10,9 @@ opts.secretOrKey    = keys.secretOrKey
 
 module.exports = passport => {
 	passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
-			User.findById(jwt_payload.id)
-				.then(user => user ? done(null, user) : done(null, false))
-				.catch(err => console.log(err))
-		})
+		User.findById(jwt_payload.id)
+			.then(user => user ? done(null, user) : done(null, false))
+			.catch(err => console.log(err))
+	})
 	)
 }
--- fact-bounty-server/models/user.js
+++ fact-bounty-server/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const Schema   = mongoose.Schema
+const Schema = mongoose.Schema
 
 // Create Schema
 const UserSchema = new Schema({
@@ -21,4 +21,4 @@ const UserSchema = new Schema({
 	}
 })
 
-module.exports = User = mongoose.model('users', UserSchema)
+module.exports = mongoose.model('users', UserSchema)
