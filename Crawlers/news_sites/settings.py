0759db4e69708ceb41e09ca03ccadf68ad5ab8e5
Lasith-Niro <lasithniro.online@gmail.com>
2019-02-16 08:00:49 +0530

    mongodb connection added

--- Crawlers/news_sites/settings.py
+++ Crawlers/news_sites/settings.py
@@ -18,6 +18,14 @@ NEWSPIDER_MODULE = 'news_sites.spiders'
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'news_sites (+http://www.yourdomain.com)'
 
+#---------------------------------------------------------------------
+ITEM_PIPELINES = {
+    'news_sites.pipelines.MongodbPipeline': 100}
+MONGODB_SERVER = "localhost"
+MONGODB_PORT = 27017
+MONGODB_DB = "news_db"
+
+
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
@@ -27,7 +35,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 30
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -88,3 +96,11 @@ DOWNLOAD_DELAY = 3
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+CONCURRENT_REQUESTS = 100
+CONCURRENT_REQUESTS_PER_DOMAIN = 5
+AUTOTHROTTLE_TARGET_CONCURRENCY = 1
+
+COOKIES_ENABLED = True
+
+DUPEFILTER_DEBUG = True
