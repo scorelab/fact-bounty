278e5e0159b39e65528f2c984c87f9c94e45243d
root5533 <ymuraad@gmail.com>
2019-02-19 15:23:24 +0530

    Update database with user vote

--- fact-bounty-client/src/pages/main/reducers/postReducers.js
+++ fact-bounty-client/src/pages/main/reducers/postReducers.js
@@ -1,4 +1,4 @@
-import { FETCH_POSTS, LOADING, APPROVE_VOTE_COMPLETE, VOTE_ERROR } from '../actions/postActions';
+import { FETCH_POSTS, LOADING, APPROVE_VOTE_COMPLETE, VOTE_ERROR, FAKE_VOTE_COMPLETE, MIX_VOTE_COMPLETE } from '../actions/postActions';
 
 const initialState = {
     items: [],
@@ -25,6 +25,28 @@ export default function (state = initialState, action) {
                 items: state.items.map((post) => {
                     if (post._id === action.id) {
                         post.approved_count += 1;
+                    }
+                    return post
+                })
+            }
+        case FAKE_VOTE_COMPLETE:
+            return {
+                ...state,
+                loading: false,
+                items: state.items.map((post) => {
+                    if (post._id === action.id) {
+                        post.fake_count += 1;
+                    }
+                    return post
+                })
+            }
+        case MIX_VOTE_COMPLETE:
+            return {
+                ...state,
+                loading: false,
+                items: state.items.map((post) => {
+                    if (post._id === action.id) {
+                        post.mixedvote_count += 1;
                     }
                     return post
                 })
--- fact-bounty-server/routes/index.js
+++ fact-bounty-server/routes/index.js
@@ -10,8 +10,8 @@ var users = require('./api/user')
 router.get('/api/stories/all', stories.storyAll)
 router.get('/api/stories/get-range/:page', stories.storyRange)
 router.post('/api/stories/change-upvote-count', stories.storyChangeUpvoteCount)
-router.get('/api/stories/change-downvote-count', stories.storyChangeDownvoteCount)
-router.get('/api/stories/change-mixedvote-count', stories.storyChangeDownvoteCount)
+router.post('/api/stories/change-downvote-count', stories.storyChangeDownvoteCount)
+router.post('/api/stories/change-mixedvote-count', stories.storyChangeDownvoteCount)
 
 // -> /api/users/
 router.post('/api/users/register', users.userRegister)
