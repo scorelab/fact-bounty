47a157a3bd2cfc6aea97502373bfb3d177627329
Anmol Bansal <abansal@ec.iitr.ac.in>
2019-02-24 01:52:33 +0530

    Add eslint to react and fix corresponding linting errors.

---
+++ fact-bounty-client/.eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        "no-unused-vars": ["off", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
+        "no-console": "off",
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+				]
+		},
+		"settings": {
+			"react": {
+				"createClass": "createReactClass",
+				"pragma": "React",
+				"version": "detect",
+				"flowVersion": "0.53"
+			}
+		}
+};
--- fact-bounty-client/src/core/authReducers.js
+++ fact-bounty-client/src/core/authReducers.js
@@ -9,18 +9,18 @@ const initialState = {
 
 export default function (state = initialState, action) {
 	switch (action.type) {
-		case SET_CURRENT_USER:
-			return {
-				...state,
-				isAuthenticated: !isEmpty(action.payload),
-				user: action.payload
-			}
-		case USER_LOADING:
-			return {
-				...state,
-				loading: true
-			}
-		default:
-			return state
+	case SET_CURRENT_USER:
+		return {
+			...state,
+			isAuthenticated: !isEmpty(action.payload),
+			user: action.payload
+		}
+	case USER_LOADING:
+		return {
+			...state,
+			loading: true
+		}
+	default:
+		return state
 	}
 }
