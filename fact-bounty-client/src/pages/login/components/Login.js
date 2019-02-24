17384bb94dfa018bdc1a709e864890add83e422c
danushka96 <danushkaherath96@gmail.com>
2019-02-24 10:10:41 +0530

    add material classes

--- fact-bounty-client/src/pages/login/components/Login.js
+++ fact-bounty-client/src/pages/login/components/Login.js
@@ -4,16 +4,54 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import classnames from 'classnames'
 
+import compose from 'recompose/compose';
+import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import withStyles from '@material-ui/core/styles/withStyles';
 
 import { loginUser } from '../actions/authActions'
 import '../styles/login.sass';
+
+const styles = theme => ({
+	main: {
+	  width: 'auto',
+	  display: 'block', // Fix IE 11 issue.
+	  marginLeft: theme.spacing.unit * 3,
+	  marginRight: theme.spacing.unit * 3,
+	  [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+		width: 400,
+		marginLeft: 'auto',
+		marginRight: 'auto',
+	  },
+	},
+	paper: {
+	  marginTop: theme.spacing.unit * 12,
+	  display: 'flex',
+	  flexDirection: 'column',
+	  alignItems: 'center',
+	  padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+	},
+	avatar: {
+	  margin: theme.spacing.unit,
+	  backgroundColor: theme.palette.secondary.main,
+	},
+	form: {
+	  width: '100%', // Fix IE 11 issue.
+	  marginTop: theme.spacing.unit,
+	},
+	submit: {
+	  marginTop: theme.spacing.unit * 3,
+	},
+  });
 
 class Login extends Component {
 	constructor() {
@@ -58,13 +96,16 @@ class Login extends Component {
 	render() {
 		const { errors } = this.state
 		return (
-			<main>
+			<main className = {this.props.classes.main}>
 			<CssBaseline />
-			<Paper>
+			<Paper className= {this.props.classes.paper}>
+				<Avatar className={this.props.classes.avatar}>
+				<LockOutlinedIcon />
+				</Avatar>
 			  <Typography component="h1" variant="h5">
 				Login
 			  </Typography>
-			  <form noValidate onSubmit={this.onSubmit}>
+			  <form noValidate onSubmit={this.onSubmit} className = {this.props.classes.form}>
 				<FormControl margin="normal" required fullWidth>
 				  <InputLabel htmlFor="email">Email Address</InputLabel>
 				  <Input onChange={this.onChange}
@@ -97,7 +138,8 @@ class Login extends Component {
 								</span>
 				</FormControl>
 				
-				<Button type="submit" fullWidth variant="contained" color="primary">
+				<Button type="submit" fullWidth variant="contained" 
+					color="primary" className = {this.props.classes.submit}>
 				  Login
 				</Button>
 			  </form>
@@ -119,7 +161,12 @@ const mapStateToProps = state => ({
 	errors: state.errors
 })
 
-export default connect(
-	mapStateToProps,
-	{ loginUser }
-)(Login)
+export default compose(
+    withStyles(styles, {
+        name: 'Login',
+    }),
+    connect(
+		mapStateToProps,
+		{ loginUser }
+	)
+)(Login);
--- fact-bounty-client/src/pages/register/components/Register.js
+++ fact-bounty-client/src/pages/register/components/Register.js
@@ -4,16 +4,52 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import classnames from 'classnames'
 
+import compose from 'recompose/compose';
+import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import FormControl from '@material-ui/core/FormControl';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import withStyles from '@material-ui/core/styles/withStyles';
 
 import { registerUser } from '../actions/newUserActions';
 import "../styles/register.sass"
+
+const styles = theme => ({
+	main: {
+	  width: 'auto',
+	  display: 'block', // Fix IE 11 issue.
+	  marginLeft: theme.spacing.unit * 3,
+	  marginRight: theme.spacing.unit * 3,
+	  [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+		width: 400,
+		marginLeft: 'auto',
+		marginRight: 'auto',
+	  },
+	},
+	paper: {
+	  marginTop: theme.spacing.unit * 12,
+	  display: 'flex',
+	  flexDirection: 'column',
+	  alignItems: 'center',
+	  padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+	},
+	avatar: {
+	  margin: theme.spacing.unit,
+	  backgroundColor: theme.palette.secondary.main,
+	},
+	form: {
+	  width: '100%', // Fix IE 11 issue.
+	  marginTop: theme.spacing.unit,
+	},
+	submit: {
+	  marginTop: theme.spacing.unit * 3,
+	},
+  });
 
 class Register extends Component {
 	constructor() {
@@ -59,13 +95,16 @@ class Register extends Component {
 	render() {
 		const { errors } = this.state
 		return (
-			<main>
+			<main className = {this.props.classes.main}>
 			<CssBaseline />
-			<Paper>
+			<Paper className= {this.props.classes.paper}>
+				<Avatar className={this.props.classes.avatar}>
+				<LockOutlinedIcon />
+				</Avatar>
 			  <Typography component="h1" variant="h5">
 				Sign Up
 			  </Typography>
-			  <form noValidate onSubmit={this.onSubmit}>
+			  <form noValidate onSubmit={this.onSubmit} className = {this.props.classes.form}>
 			  <FormControl margin="normal" required fullWidth>
 				  <InputLabel htmlFor="email">Name</InputLabel>
 				  <Input onChange={this.onChange}
@@ -118,7 +157,8 @@ class Register extends Component {
 					<span className="red-text">{errors.password2}</span>
 				</FormControl>
 				
-				<Button type="submit" fullWidth variant="contained" color="primary">
+				<Button type="submit" fullWidth variant="contained" 
+					color="primary" className = {this.props.classes.submit}>
 				  Sign Up
 				</Button>
 			  </form>
@@ -140,7 +180,12 @@ const mapStateToProps = state => ({
 	errors: state.errors
 })
 
-export default connect(
-	mapStateToProps,
-	{ registerUser }
-)(withRouter(Register))
+export default compose(
+    withStyles(styles, {
+        name: 'Register',
+    }),
+    connect(
+		mapStateToProps,
+		{ registerUser }
+	)
+)(Register);
