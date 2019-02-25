55262591c97d56ef3eb74e245b4ef206cad69fe2
akata01 <akata0612@gmail.com>
2019-02-25 14:50:05 -0500

    eslint fix

--- fact-bounty-client/.eslintrc.js
+++ fact-bounty-client/.eslintrc.js
@@ -1,43 +1,31 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true
+  },
+  extends: ["react-app", "eslint:recommended", "plugin:react/recommended"],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  rules: {
+    "no-unused-vars": [
+      "off",
+      { vars: "all", args: "after-used", ignoreRestSiblings: false }
     ],
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "rules": {
-        "no-unused-vars": ["off", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
-        "no-console": "off",
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "never"
-				]
-		},
-		"settings": {
-			"react": {
-				"createClass": "createReactClass",
-				"pragma": "React",
-				"version": "detect",
-				"flowVersion": "0.53"
-			}
-		}
+    "no-console": "off",
+    indent: ["error", "tab"],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single"],
+    semi: ["error", "never"]
+  },
+  settings: {
+    react: {
+      createClass: "createReactClass",
+      pragma: "React",
+      version: "detect",
+      flowVersion: "0.53"
+    }
+  }
 };
