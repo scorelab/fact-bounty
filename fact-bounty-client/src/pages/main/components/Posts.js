91cbb7c5070e73e2a20ea55d2266850f2a2eb761
akata01 <akata0612@gmail.com>
2019-02-26 03:53:21 -0500

    flow setup

--- fact-bounty-client/src/pages/main/components/Posts.js
+++ fact-bounty-client/src/pages/main/components/Posts.js
@@ -1,50 +1,63 @@
-import React, { Component } from 'react'
-import '../Posts.sass'
-import Post from './Post'
-import { connect } from 'react-redux'
-import { fetchPosts } from '../actions/postActions'
-import InfiniteScroll from 'react-infinite-scroller'
+// @flow
 
-class Posts extends Component {
+import React, { Component } from "react";
+import "../Posts.sass";
+import Post from "./Post";
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions/postActions";
+import InfiniteScroll from "react-infinite-scroller";
 
-	loadItems() {
-		if (!this.props.loading) {
-			this.props.fetchPosts(this.props.nextPage)
-		}
-	}
+type Props = {
+  nextPage: number,
+  loading: boolean,
+  hasMore: boolean,
+  fetchPosts: (nextPage: number) => {},
+  posts: Array<{}>
+};
 
-	render() {
-		const loader = <div className="loader" key={0}>Loading ...</div>
-		var items = []
-		this.props.posts.map(post => {
-			items.push(
-				<Post key={post._id} post={post} />
-			)
-		})
-		return (
-			<InfiniteScroll
-				pageStart={0}
-				loadMore={this.loadItems.bind(this)}
-				hasMore={this.props.hasMore}
-				loader={loader}>
-				<div className="postLayout">
-					<div></div>
-					<div className="postColumn">
-						{items}
-					</div>
-					<div></div>
-				</div>
-			</InfiniteScroll>
-		)
+class Posts extends Component<Props> {
+  static defaultProps = {};
+  loadItems() {
+    if (!this.props.loading) {
+      this.props.fetchPosts(this.props.nextPage);
+    }
+  }
 
-	}
+  render() {
+    const loader = (
+      <div className="loader" key={0}>
+        Loading ...
+      </div>
+    );
+    var items = [];
+    this.props.posts.map(post => {
+      items.push(<Post key={post._id} post={post} />);
+    });
+    return (
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={this.loadItems.bind(this)}
+        hasMore={this.props.hasMore}
+        loader={loader}
+      >
+        <div className="postLayout">
+          <div />
+          <div className="postColumn">{items}</div>
+          <div />
+        </div>
+      </InfiniteScroll>
+    );
+  }
 }
 
 const mapStatetoProps = state => ({
-	posts: state.posts.items,
-	nextPage: state.posts.nextPage,
-	hasMore: state.posts.hasMore,
-	loading: state.posts.loading
-})
+  posts: state.posts.items,
+  nextPage: state.posts.nextPage,
+  hasMore: state.posts.hasMore,
+  loading: state.posts.loading
+});
 
-export default connect(mapStatetoProps, { fetchPosts })(Posts)
+export default connect(
+  mapStatetoProps,
+  { fetchPosts }
+)(Posts);
