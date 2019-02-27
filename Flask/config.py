63af1f4dee1f339abf94ead150e1582c9022f257
Saad Patel <44060028+Saad2714@users.noreply.github.com>
2019-02-28 02:38:35 +0530

    Add files via upload

---
+++ Flask/config.py
@@ -0,0 +1,7 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+# Database stuff we will add soon
