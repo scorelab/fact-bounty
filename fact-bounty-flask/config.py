4454ec2aeacca02399a216bcd2ad013845740df5
Anmol Bansal <abansal@ec.iitr.ac.in>
2019-03-06 19:08:57 +0530

    Initialize tests
    created tests folder to add tests
    add config of tests and docker
    update boot file

--- fact-bounty-flask/config.py
+++ fact-bounty-flask/config.py
@@ -22,13 +22,35 @@ class DevelopmentConfig(Config):
     """Development Congigurations"""
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') \
-                              or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+        or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
     # needs to be removed in further versions
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+class DockerConfig(Config):
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+
+        # log to stderr
+        import logging
+        from logging import StreamHandler
+        file_handler = StreamHandler()
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
 
 config = {
     'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'docker': DockerConfig,
 
     'default': DevelopmentConfig
 }
