39c52eb2d4fa507065d4b5459c997958d9799879
Anmol Bansal <abansal@ec.iitr.ac.in>
2019-03-19 16:48:08 +0530

    Include code coverage
    Add support of code coverage in tests.
    Increase verbosity of tests.

--- fact-bounty-flask/config.py
+++ fact-bounty-flask/config.py
@@ -28,6 +28,7 @@ class DevelopmentConfig(Config):
 
 class TestingConfig(Config):
     TESTING = True
+    WTF_CSRF_ENABLED = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
