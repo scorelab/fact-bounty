a25ab3efaeb05b8373103f6772a6e015d7b7c8c2
Tushar Varshney <juniorvarshney19@gmail.com>
2019-03-19 22:27:00 +0530

    proxy port changed, login form error handling and prettier added

--- fact-bounty-client/src/pages/register/actions/newUserActions.js
+++ fact-bounty-client/src/pages/register/actions/newUserActions.js
@@ -6,18 +6,17 @@ export const registerUser = (userData, history) => dispatch => {
   axios
     .post("/api/users/register", userData)
     .then(res => {
-      if(res.status === 202){
+      if (res.status === 202) {
         dispatch({
           type: GET_ERRORS,
           payload: res.data.message
-        })
-      }
-      else{
-        history.push('/login')
+        });
+      } else {
+        history.push("/login");
       }
     }) // re-direct to login on successful register
     .catch(err => {
-      if(err&&err.response){
+      if (err && err.response) {
         let payload = err.response.data;
         if (typeof payload === "string") {
           payload = { fetch: err.response.data };
