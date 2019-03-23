c582bbcf9607f0462fbc477a98e6f3638a69febb
Tushar Varshney <juniorvarshney19@gmail.com>
2019-03-23 06:57:02 +0530

    structurize the __init__.py file

--- fact-bounty-flask/__init__.py
+++ fact-bounty-flask/__init__.py
@@ -19,13 +19,25 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
 
+    register_extensions(app)
+    register_blueprint(app)
+
+    return app
+
+def register_extensions(app):
+    """Register Flask extensions"""
     CORS(app)
     db.init_app(app)
     login_manager.init_app(app)
     pagedown.init_app(app)
     app.elasticsearch = Elasticsearch()
+    return None
 
+def register_blueprint(app):
+    """Register Flask blueprints."""
     from .api import api as api_blueprint
     app.register_blueprint(api_blueprint, url_prefix='/api')
+    return None
 
-    return app
+def register_commands(app):
+    """Regiter Click commands"""
