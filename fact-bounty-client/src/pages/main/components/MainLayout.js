e02d205a4321715d6a58d1cb5e8cce068a15fa13
Kartik Pandey <kartikpandey2@gmail.com>
2019-03-25 03:43:53 +0530

    feat: 404 page #241

--- fact-bounty-client/src/pages/main/components/MainLayout.js
+++ fact-bounty-client/src/pages/main/components/MainLayout.js
@@ -6,7 +6,7 @@ import About from "../../about/About";
 class MainLayout extends Component {
   render() {
     return (
-      <div style={{ paddingTop: "74px" }}>
+      <div style={{ paddingTop: "65px" }}>
         <Route exact path={"/"} component={Posts} />
         <Route path="/about" component={About} />
       </div>
