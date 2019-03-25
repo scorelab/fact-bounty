e83c49550a7395bea7650f4a0d12a38cd31b4c5e
Kartik Pandey <kartikpandey2@gmail.com>
2019-03-25 21:31:47 +0530

    fix: #243 redux devtools optional

--- fact-bounty-client/src/core/store.js
+++ fact-bounty-client/src/core/store.js
@@ -4,13 +4,11 @@ import rootReducer from "./rootReducer";
 
 const initialState = {};
 const middleware = [thunk];
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeEnhancers(applyMiddleware(...middleware))
 );
 
 export default store;
