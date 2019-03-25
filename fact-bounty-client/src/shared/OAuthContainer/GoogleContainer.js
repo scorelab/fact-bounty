73008044878c9a4581be107c9b9875f12e307c8d
Kartik Pandey <kartikpandey2@gmail.com>
2019-03-26 01:41:37 +0530

    fix: google login

--- fact-bounty-client/src/shared/OAuthContainer/GoogleContainer.js
+++ fact-bounty-client/src/shared/OAuthContainer/GoogleContainer.js
@@ -1,16 +1,29 @@
+<<<<<<< HEAD
 import React from "react";
+=======
+import React, { Fragment, Component } from "react";
+import Button from "@material-ui/core/Button";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+>>>>>>> fix: google login
 import { GoogleLogin } from "react-google-login";
 
 import { OauthUser } from "./actions/OAuthUser";
 
+<<<<<<< HEAD
 import "./index.css";
+=======
+import Toast from "../components/Snackbar";
+import styles from "./index.css";
+>>>>>>> fix: google login
 
-class GoogleContainer extends React.Component {
+class GoogleContainer extends Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
       status: "unknown",
-      errors: null
+      errors: null,
+      openToast: false
     };
   }
 
@@ -20,26 +33,60 @@ class GoogleContainer extends React.Component {
       name: res.profileObj.name,
       email: res.profileObj.email
     };
-    OauthUser(creds);
+    this.props.OauthUser(creds);
   };
 
   errors = err => {
-    console.log(err);
+    this.setState({ errors: err, openToast: true });
+  };
+
+  closeToast = () => {
+    this.setState({ openToast: false });
   };
 
   render() {
+    const { openToast } = this.state;
     return (
-      <GoogleLogin
-        className="googleSignin"
-        style={{ width: "15rem" }}
-        buttonText="Login with google"
-        clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-        onSuccess={this.responseGoogle}
-        onFailure={this.errors}
-        icon="true"
-      />
+      <Fragment>
+        {openToast && (
+          <Toast
+            open={openToast}
+            onClose={this.closeToast}
+            message="Something went wrong, Try again later"
+            variant="error"
+          />
+        )}
+        <GoogleLogin
+          render={renderProps => (
+            <Button className={styles.fbauth} onClick={renderProps.onClick}>
+              <img
+                style={{ width: "14px", marginRight: "7px" }}
+                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                alt="icon"
+              />
+              Login with Google
+            </Button>
+          )}
+          clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+          onSuccess={this.responseGoogle}
+          onFailure={this.errors}
+        />
+      </Fragment>
     );
   }
 }
 
-export default GoogleContainer;
+GoogleContainer.propTypes = {
+  OauthUser: PropTypes.func.isRequired
+};
+
+const mapStateToProps = null;
+
+const mapDispatchToProps = {
+  OauthUser
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(GoogleContainer);
--- fact-bounty-client/src/shared/OAuthContainer/index.js
+++ fact-bounty-client/src/shared/OAuthContainer/index.js
@@ -1,38 +1,14 @@
 import React from "react";
 
+import FacebookContainer from "./FacebookContainer";
+import GoogleContainer from "./GoogleContainer";
 import "./index.css";
 
-class OauthContainer extends React.Component {
-  constructor(props, context) {
-    super(props, context);
-    this.state = {
-      FacebookContainer: null,
-      GoogleContainer: null
-    };
-  }
-
-  async componentDidMount() {
-    const { default: FacebookContainer } = await import("./FacebookContainer");
-    const { default: GoogleContainer } = await import("./GoogleContainer");
-    this.setState({
-      loading: false,
-      FacebookContainer: (
-        // <FacebookContainer class={styles.facebookIconSignup} />
-        <FacebookContainer />
-      ),
-      // GoogleContainer: <GoogleContainer class={styles.googleIconSignup} />
-      GoogleContainer: <GoogleContainer />
-    });
-  }
-
-  render() {
-    return (
-      <div className="socialIconsSignup">
-        {this.state.FacebookContainer}
-        {this.state.GoogleContainer}
-      </div>
-    );
-  }
-}
+const OauthContainer = () => (
+  <div className="socialIconsSignup">
+    <FacebookContainer />
+    <GoogleContainer />
+  </div>
+);
 
 export default OauthContainer;
