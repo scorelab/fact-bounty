58d7e7f95543e7d5325877503b9935e5d6318fe5
Kartik Pandey <kartikpandey2@gmail.com>
2019-03-26 19:57:20 +0530

    fix: routes

--- fact-bounty-client/src/pages/main/components/MainLayout.js
+++ fact-bounty-client/src/pages/main/components/MainLayout.js
@@ -1,18 +1,7 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
 
 import Posts from "./Posts";
-import About from "../../about/About";
 
-class MainLayout extends Component {
-  render() {
-    return (
-      <div style={{ paddingTop: "65px" }}>
-        <Route exact path={"/"} component={Posts} />
-        <Route path="/about" component={About} />
-      </div>
-    );
-  }
-}
+const MainLayout = () => <Posts />;
 
 export default MainLayout;
