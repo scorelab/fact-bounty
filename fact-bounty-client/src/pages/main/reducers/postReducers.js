a1ee0808f21ed3a6118d205021965e7f96cc6270
root5533 <ymuraad@gmail.com>
2019-04-15 18:59:38 +0530

    Mix vote per post per user

--- fact-bounty-client/src/pages/main/reducers/postReducers.js
+++ fact-bounty-client/src/pages/main/reducers/postReducers.js
@@ -68,7 +68,9 @@ export default function(state = initialState, action) {
         loading: false,
         items: state.items.map(post => {
           if (post._id === action.id) {
-            post.mixedvote_count += 1;
+            post.approved_count = action.approved_count;
+            post.fake_count = action.fake_count;
+            post.mixedvote_count = action.mixedvote_count;
           }
           return post;
         })
