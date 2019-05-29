0e5d14b59eadabe81024d27a886a7e5e11e17025
anmolbansal1 <abansal@ec.iitr.ac.in>
2019-05-29 13:25:00 +0530

    Add new spiders DailyMirrorLK, economynext and ft

--- Crawlers/news_sites/spiders/ft.py
+++ Crawlers/news_sites/spiders/ft.py
@@ -2,20 +2,25 @@ import scrapy
 from ..items import NewsSitesItem
 
 
-class EconomyNextSpider(scrapy.Spider):
-    name = "economynext"
-    allowed_domains = ["economynext.com"]
-    start_urls = ['http://www.economynext.com/']
+class FtSpider(scrapy.Spider):
+    name = "ft"
+    allowed_domains = ["ft.lk"]
+    start_urls = ['http://www.ft.lk']
 
     def parse(self, response):
-        categories = response.css('.sub-menu div:nth-child(1) a::attr(href)').extract()
+        temp = response.css('.bottom-text::attr(href)').extract()
+
+        # remove duplicate categories
+        categories = [] 
+        [categories.append(x) for x in temp if x not in categories]
+
         for category in categories:
             yield response.follow(category, callback=self.parse_category)
     
     def parse_category(self, response):
         # extract all news articles urls
-        temp = response.css('.related-block a:nth-child(1)::attr(href)').extract()
-
+        temp = response.css('.cat-header').xpath('../@href').extract()
+        
         # remove duplicates
         news_urls = [] 
         [news_urls.append(x) for x in temp if x not in news_urls]
@@ -25,22 +30,22 @@ class EconomyNextSpider(scrapy.Spider):
             yield response.follow(news_url, callback=self.parse_article)
         
         # proceed to next page
-        next_page = response.css('.next::attr(href)').extract_first()
+        next_page = response.css('.active+ li a::attr(href)').extract_first()
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
     
     def parse_article(self, response):
         item = NewsSitesItem()
 
-        item['author'] = 'EconomyNext'
-        item['title'] = response.css('.article-title::text').extract_first()
-        date  = response.css('.article-title+ h2::text').extract_first()
+        item['author'] = 'FT'
+        item['title'] = response.css('.inner-header::text').extract_first()
+        date  = response.css('.inner-ft-text img::text').extract_first()
         if date is not None:
-            item['date'] = date.split('|')[0]
+            item['date'] = date.split('/')[-1]
         else:
             item['date'] = None
         item['imageLink'] = response.css('.article-photo .set-image-border::attr(src)').extract_first()
-        item['source'] = 'https://economynext.com/'
-        item['content'] = response.css('p::text').extract_first()
+        item['source'] = 'http://ft.lk/'
+        item['content'] = ' \n '.join(response.css('.inner-ft-text p::text').extract())
 
         yield item
