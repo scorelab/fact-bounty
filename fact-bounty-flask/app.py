952a91cb3355a0706fdf42031ca65753471dadd2
Devon Wijesinghe <devon@Devons-MacBook-Pro.local>
2019-06-04 17:40:02 +0530

    Adds app.yaml file / prepare for GCP deploy

--- fact-bounty-flask/app.py
+++ fact-bounty-flask/app.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from flask import render_template
 import coverage
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
@@ -11,3 +12,7 @@ import sys
 from api.app import create_app
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
+@app.route('/')
+def default_route():
+        return render_template('index.html')
