8f18e43bb7053f14715341489993993f6df482bf
anmolbansal1 <abansal@ec.iitr.ac.in>
2019-06-04 16:41:44 +0530

    add migration command, shell context processor and fix stories route

--- fact-bounty-flask/api/extensions.py
+++ fact-bounty-flask/api/extensions.py
@@ -3,9 +3,9 @@ from flask_migrate import Migrate
 from flask_pagedown import PageDown
 from flask_sqlalchemy import SQLAlchemy
 
-
 db = SQLAlchemy()
 pagedown = PageDown()
 
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
+migrate = Migrate()
