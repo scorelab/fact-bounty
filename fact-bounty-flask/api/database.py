e9f0e921c9bce5be791cf9288d3ffe0c273c0871
anmolbansal1 <abansal@ec.iitr.ac.in>
2019-06-04 14:31:31 +0530

    Refactor flask app structure
    Move all functions to sub directory api
    Seperate MVC structure of stories and users
    Add database.py to access db related functions
    FIx circular dependencies

---
+++ fact-bounty-flask/api/database.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Database module, including the SQLAlchemy database object and DB-related utilities."""
+from .extensions import db
+
+# Alias common SQLAlchemy names
+Column = db.Column
+Model = db.Model
