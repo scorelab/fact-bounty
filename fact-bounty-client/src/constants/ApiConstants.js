eee63d5fcb2af0ce7bd57c81eb86157f43007353
Devon Wijesinghe <devon@Devons-MacBook-Pro.local>
2019-06-08 17:33:47 +0530

    Adds postbuild script and prod api url constant

--- fact-bounty-client/src/constants/ApiConstants.js
+++ fact-bounty-client/src/constants/ApiConstants.js
@@ -1,5 +1,6 @@
-const API_URL_DEV = 'http://localhost:5000'
+const API_URL_LOCAL = 'http://localhost:5000'
+const API_URL_PROD = 'https://personal-fact-bounty.appspot.com'
 
 export const APIConstantsDev = {
-  API_URL: API_URL_DEV
+  API_URL: API_URL_PROD
 }
