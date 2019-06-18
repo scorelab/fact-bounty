421e6705fec689567fd91ed8bae4610da2dfd354
Devon Wijesinghe <devon@Devons-MacBook-Pro.local>
2019-06-18 15:01:54 +0530

    Adds about section

--- fact-bounty-client/src/pages/Landing/Landing.jsx
+++ fact-bounty-client/src/pages/Landing/Landing.jsx
@@ -7,6 +7,7 @@ class Landing extends Component {
   render() {
     return (
       <div className="landing-container">
+        {/* ============= HEADER SECTION ============= */}
         <div className="container">
           <div className="header">
             <div className="row">
@@ -19,6 +20,49 @@ class Landing extends Component {
               </div>
               <div className="col-md right-section">
                 <img className="header-img" src={headerImg} alt="News" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <hr className="divider" />
+
+        {/* ============= HEADER SECTION ============= */}
+        <div className="container">
+          <div className="about">
+            <h1>About Factbounty</h1>
+
+            <div className="row">
+              <div className="col-md">
+                <div className="about-block">
+                  <label>1</label>AWESOME FEATURE
+                  <h3>Lorem ipsum dolor</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non dignissim lectus, vel mattis lacus. Vivamus non dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non dignissim lectus, vel mattis lacus. Vivamus non dignissim.</p>
+                </div>
+              </div>
+              <div className="col-md">
+                <div className="about-block">
+                  <label>1</label>AWESOME FEATURE
+                  <h3>Lorem ipsum dolor</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non dignissim lectus, vel mattis lacus. Vivamus non dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non dignissim lectus, vel mattis lacus. Vivamus non dignissim.</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md">
+                <div className="about-block">
+                  <label>1</label>AWESOME FEATURE
+                  <h3>Lorem ipsum dolor</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non dignissim lectus, vel mattis lacus. Vivamus non dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non dignissim lectus, vel mattis lacus. Vivamus non dignissim.</p>
+                </div>
+              </div>
+              <div className="col-md">
+                <div className="about-block">
+                  <label>1</label>AWESOME FEATURE
+                  <h3>Lorem ipsum dolor</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non dignissim lectus, vel mattis lacus. Vivamus non dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non dignissim lectus, vel mattis lacus. Vivamus non dignissim.</p>
+                </div>
               </div>
             </div>
           </div>
