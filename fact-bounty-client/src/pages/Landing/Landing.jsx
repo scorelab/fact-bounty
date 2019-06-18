3cdff391c6f0f20de051a71086dcc80b78297b4e
Devon Wijesinghe <devon@Devons-MacBook-Pro.local>
2019-06-18 13:45:24 +0530

    Adds landing header style

--- fact-bounty-client/src/pages/Landing/Landing.jsx
+++ fact-bounty-client/src/pages/Landing/Landing.jsx
@@ -1,16 +1,26 @@
 import React, { Component } from 'react'
+import headerImg from '../../assets/img/headerImg.png'
+import Button from '@material-ui/core/Button'
 import './style.sass'
 
 class Landing extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md">
-            <h1>Welcome to Fact Bounty</h1>
-          </div>
-          <div className="col-md">
-            <img src="" alt="Header Image" />
+      <div className="landing-container">
+        <div className="container">
+          <div className="header">
+            <div className="row">
+              <div className="col-md left-section">
+                <h1>Welcome to Fact Bounty</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non dignissim lectus, vel mattis lacus. Vivamus non dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                <Button variant="contained" color="primary" style={{ width: 120 }}>
+                  EXPLORE
+              </Button>
+              </div>
+              <div className="col-md right-section">
+                <img className="header-img" src={headerImg} alt="News" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
