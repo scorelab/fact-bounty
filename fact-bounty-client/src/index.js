3590bb05f66739dd2ee5777363dff6193af180f0
Devon Wijesinghe <devon@Devons-MacBook-Pro.local>
2019-06-18 12:40:51 +0530

    Adds styles folder and adds bootstrap/font-awsome

--- fact-bounty-client/src/index.js
+++ fact-bounty-client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.scss'
+import './styles/style.sass'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
--- fact-bounty-client/src/pages/Landing/Landing.jsx
+++ fact-bounty-client/src/pages/Landing/Landing.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import './style.sass'
 
 class Landing extends Component {
   render() {
     return (
-      <div className="landing-container">
-        <h1>Welcome to Fact Bounty</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-md">
+            <h1>Welcome to Fact Bounty</h1>
+          </div>
+          <div className="col-md">
+            <img src="" alt="Header Image" />
+          </div>
+        </div>
       </div>
     )
   }
