52cd1ea1dfeee6804f476ed7f16210bb75527b10
anmolbansal1 <abansal@ec.iitr.ac.in>
2019-06-21 20:50:28 +0530

    feat: add SMTP server
    setup smtp server with Flask-Mail extension
    add error mailing in production config

--- fact-bounty-flask/api/extensions.py
+++ fact-bounty-flask/api/extensions.py
@@ -1,9 +1,11 @@
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_mail import Mail
 from flask_pagedown import PageDown
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+mail = Mail()
 pagedown = PageDown()
 
 login_manager = LoginManager()
