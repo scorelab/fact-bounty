0c3e370ff416cfcfd988ad492b2aeb7e56c90b24
Devon Wijesinghe <devon@Devons-MacBook-Pro.local>
2019-06-26 23:44:05 +0530

    Completes landing page styles

--- fact-bounty-client/src/components/NavBar/NavBar.jsx
+++ fact-bounty-client/src/components/NavBar/NavBar.jsx
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
 import SwipeableDrawer from '../SwipeableDrawer'
@@ -80,8 +79,8 @@ class NavBar extends Component {
               {auth.isAuthenticated ? (
                 <PrivateLinks handleLogout={this.handleLogout} />
               ) : (
-                  <PublicLinks />
-                )}
+                <PublicLinks />
+              )}
             </div>
           </Toolbar>
         </AppBar>
--- fact-bounty-client/src/pages/Landing/Landing.jsx
+++ fact-bounty-client/src/pages/Landing/Landing.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import headerImg from '../../assets/img/headerImg.png'
 import Button from '@material-ui/core/Button'
 import Footer from '../../components/Footer'
+import patch2 from '../../assets/img/patch2.png'
+import patch3 from '../../assets/img/patch3.png'
 import './style.sass'
 
 class Landing extends Component {
-
   componentDidMount() {
     this.initHashScroll()
   }
@@ -26,16 +28,34 @@ class Landing extends Component {
   render() {
     return (
       <div className="landing-container">
+        <div className="patch-wrapper-2">
+          <img src={patch2} alt="patch" />
+        </div>
+        <div className="patch-wrapper-3">
+          <img src={patch3} alt="patch" />
+        </div>
         {/* ============= HEADER SECTION ============= */}
         <div className="container">
           <div className="header">
             <div className="row">
               <div className="col-md left-section">
-                <h1>Welcome to Fact Bounty</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non dignissim lectus, vel mattis lacus. Vivamus non dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-                <Button variant="contained" color="primary" style={{ width: 120 }}>
-                  EXPLORE
-                </Button>
+                <h1>
+                  Welcome to <b>Fact Bounty</b>
+                </h1>
+                <p>
+                  Fact Bounty is a crowd sourced based fact checking platform.
+                  Using Fact Bounty, you will be able to find out the truth and
+                  minimize spread of false news and roumors
+                </p>
+                <Link to="/#about">
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    style={{ width: 120 }}
+                  >
+                    EXPLORE
+                  </Button>
+                </Link>
               </div>
               <div className="col-md right-section">
                 <img className="header-img" src={headerImg} alt="News" />
@@ -49,21 +69,28 @@ class Landing extends Component {
         {/* ============= ABOUT SECTION ============= */}
         <div className="container" id="about">
           <div className="about">
-            <h1>About Factbounty</h1>
+            <h1>About Fact Bounty</h1>
 
             <div className="row">
               <div className="col-md">
                 <div className="about-block">
-                  <label>1</label>AWESOME FEATURE
-                  <h3>Lorem ipsum dolor</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non dignissim lectus, vel mattis lacus. Vivamus non dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non dignissim lectus, vel mattis lacus. Vivamus non dignissim.</p>
+                  <label>1</label>SEARCH
+                  <h3>Search News Posts</h3>
+                  <p>
+                    Search for news posts in Fact Bounty and find out which
+                    posts are real and which is fake
+                  </p>
                 </div>
               </div>
               <div className="col-md">
                 <div className="about-block">
-                  <label>1</label>AWESOME FEATURE
-                  <h3>Lorem ipsum dolor</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non dignissim lectus, vel mattis lacus. Vivamus non dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non dignissim lectus, vel mattis lacus. Vivamus non dignissim.</p>
+                  <label>2</label>VOTE
+                  <h3>Vote News Posts</h3>
+                  <p>
+                    Users can upvote, down vote or put an neutral vote to new
+                    posts listed in fact bounty, which will be used to validate
+                    the facts stated in the news
+                  </p>
                 </div>
               </div>
             </div>
@@ -71,17 +98,16 @@ class Landing extends Component {
             <div className="row">
               <div className="col-md">
                 <div className="about-block">
-                  <label>1</label>AWESOME FEATURE
-                  <h3>Lorem ipsum dolor</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non dignissim lectus, vel mattis lacus. Vivamus non dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non dignissim lectus, vel mattis lacus. Vivamus non dignissim.</p>
+                  <label>3</label>VISUALIZE
+                  <h3>Visualize News Post Stats</h3>
+                  <p>
+                    View charts and graphs to see in a visual representations of
+                    roumor spread and news source validity
+                  </p>
                 </div>
               </div>
               <div className="col-md">
-                <div className="about-block">
-                  <label>1</label>AWESOME FEATURE
-                  <h3>Lorem ipsum dolor</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non dignissim lectus, vel mattis lacus. Vivamus non dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non dignissim lectus, vel mattis lacus. Vivamus non dignissim.</p>
-                </div>
+                <div className="about-block" />
               </div>
             </div>
           </div>
@@ -89,19 +115,18 @@ class Landing extends Component {
 
         {/* ============= RECENT POSTS SECTION ============= */}
         <div className="container" id="recentPosts">
-          <div className="recent-posts">
-          </div>
+          <div className="recent-posts" />
         </div>
 
         {/* ============= CONTACT US SECTION ============= */}
         <div className="container" id="contact">
           <div className="contact-us">
             <h1>Contact Us</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p>Feel free to contact us for any inquiries or feedback</p>
             <form>
               <input type="text" name="firstname" placeholder="First Name" />
               <input type="text" name="firstname" placeholder="Email" />
-              <textarea name="message" rows="5" placeholder="Your Message"/>
+              <textarea name="message" rows="5" placeholder="Your Message" />
             </form>
             <Button variant="contained" color="primary" style={{ width: 120 }}>
               SEND
