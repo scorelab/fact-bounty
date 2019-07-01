c17281d860d2b5801a727f1b943860cb9c140449
anmolbansal1 <abansal@ec.iitr.ac.in>
2019-07-01 15:31:55 +0530

    Add table creation before first api request
    Fix migrate command in api, removed migrations for now
    Add revoked token table in data-dev

--- fact-bounty-flask/app.py
+++ fact-bounty-flask/app.py
@@ -5,16 +5,13 @@ from flask import render_template
 from api.app import create_app
 from api.extensions import db
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+app = create_app(os.getenv("FLASK_CONFIG") or "default")
 
 
-@app.route('/')
+@app.route("/")
 def default_route():
-    return render_template('index.html')
-
-
-db.create_all(app=create_app(os.getenv('FLASK_CONFIG') or 'default'))
+    return render_template("index.html")
