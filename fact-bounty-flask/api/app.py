5e65a9f07ae131ca59e4556c64e32bfaa6a629c4
anmolbansal1 <abansal@ec.iitr.ac.in>
2019-07-01 15:31:55 +0530

    Add table creation before first api request
    Fix migrate command in api, removed migrations for now
    Add revoked token table in data-dev

--- fact-bounty-flask/api/app.py
+++ fact-bounty-flask/api/app.py
@@ -10,7 +10,7 @@ from flask_jwt_extended import JWTManager
 from api import commands
 from api import user, stories, crawler, util
 from api.config import config
-from api.extensions import db, mail, pagedown, login_manager
+from api.extensions import db, mail, pagedown, login_manager, migrate
 
 
 def create_app(config_name):
@@ -26,6 +26,10 @@ def create_app(config_name):
     register_shellcontext(app)
     register_commands(app)
 
+    @app.before_first_request
+    def create_tables():
+        db.create_all()
+
     return app
 
 
@@ -35,6 +39,7 @@ def register_extensions(app):
     db.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
+    migrate.init_app(app, db)
     pagedown.init_app(app)
     if os.environ.get("FLASK_CONFIG") != "production":
         es = Elasticsearch([app.config["ES_URL"]])
@@ -73,6 +78,7 @@ def register_commands(app):
     app.cli.add_command(commands.lint)
     app.cli.add_command(commands.clean)
     app.cli.add_command(commands.urls)
+    app.cli.add_command(commands.deploy)
 
 
 def register_shellcontext(app):
--- fact-bounty-flask/api/commands.py
+++ fact-bounty-flask/api/commands.py
@@ -13,58 +13,70 @@ from werkzeug.exceptions import MethodNotAllowed, NotFound
 
 
 COV = None
-if os.environ.get('FLASK_COVERAGE'):
-    COV = coverage.coverage(branch=True, include='./*')
+if os.environ.get("FLASK_COVERAGE"):
+    COV = coverage.coverage(branch=True, include="./*")
     COV.start()
 
 
 @click.command()
-@click.option('--coverage/--no-coverage', default=False,
-              help='Run tests under code coverage.')
+@click.option(
+    "--coverage/--no-coverage",
+    default=False,
+    help="Run tests under code coverage.",
+)
 def test(coverage):
     """Run the unit tests."""
-    if coverage and not os.environ.get('FLASK_COVERAGE'):
+    if coverage and not os.environ.get("FLASK_COVERAGE"):
         import subprocess
-        os.environ['FLASK_COVERAGE'] = '1'
+
+        os.environ["FLASK_COVERAGE"] = "1"
         sys.exit(subprocess.call(sys.argv))
     import unittest
-    tests = unittest.TestLoader().discover('tests')
+
+    tests = unittest.TestLoader().discover("tests")
     unittest.TextTestRunner(verbosity=2).run(tests)
     if COV:
         COV.stop()
         COV.save()
-        print('Coverage Summary:')
+        print("Coverage Summary:")
         COV.report()
         basedir = os.path.abspath(os.path.dirname(__file__))
-        covdir = os.path.join(basedir, 'tmp/coverage')
+        covdir = os.path.join(basedir, "tmp/coverage")
         COV.html_report(directory=covdir)
-        print('HTML version: file://%s/index.html' % covdir)
+        print("HTML version: file://%s/index.html" % covdir)
         COV.erase()
 
 
 @click.command()
-@click.option('-f', '--fix-imports', default=False, is_flag=True,
-              help='Fix imports using isort, before linting')
+@click.option(
+    "-f",
+    "--fix-imports",
+    default=False,
+    is_flag=True,
+    help="Fix imports using isort, before linting",
+)
 def lint(fix_imports):
     """Lint and check code style with flake8 and isort."""
-    skip = ['requirements', 'venv', 'migrations', '__pycache__']
-    root_files = glob('*.py')
+    skip = ["requirements", "venv", "migrations", "__pycache__"]
+    root_files = glob("*.py")
     root_directories = [
-        name for name in next(os.walk('.'))[1] if not name.startswith('.')]
+        name for name in next(os.walk("."))[1] if not name.startswith(".")
+    ]
     files_and_directories = [
-        arg for arg in root_files + root_directories if arg not in skip]
+        arg for arg in root_files + root_directories if arg not in skip
+    ]
 
     def execute_tool(description, *args):
         """Execute a checking tool with its arguments."""
         command_line = list(args) + files_and_directories
-        click.echo('{}: {}'.format(description, ' '.join(command_line)))
+        click.echo("{}: {}".format(description, " ".join(command_line)))
         rv = call(command_line)
         if rv != 0:
             exit(rv)
 
     if fix_imports:
-        execute_tool('Fixing import order', 'isort', '-rc')
-    execute_tool('Checking code style', 'flake8')
+        execute_tool("Fixing import order", "isort", "-rc")
+    execute_tool("Checking code style", "flake8")
 
 
 @click.command()
@@ -73,19 +85,23 @@ def clean():
 
     Borrowed from Flask-Script, converted to use Click.
     """
-    for dirpath, _, filenames in os.walk('.'):
+    for dirpath, _, filenames in os.walk("."):
         for filename in filenames:
-            if filename.endswith('.pyc') or filename.endswith('.pyo'):
+            if filename.endswith(".pyc") or filename.endswith(".pyo"):
                 full_pathname = os.path.join(dirpath, filename)
-                click.echo('Removing {}'.format(full_pathname))
+                click.echo("Removing {}".format(full_pathname))
                 os.remove(full_pathname)
 
 
 @click.command()
-@click.option('--url', default=None,
-              help='Url to test (ex. /static/image.png)')
-@click.option('--order', default='rule',
-              help='Property on Rule to order by (default: rule)')
+@click.option(
+    "--url", default=None, help="Url to test (ex. /static/image.png)"
+)
+@click.option(
+    "--order",
+    default="rule",
+    help="Property on Rule to order by (default: rule)",
+)
 @with_appcontext
 def urls(url, order):
     """Display all of the url matching routes for the project.
@@ -93,51 +109,54 @@ def urls(url, order):
     Borrowed from Flask-Script, converted to use Click.
     """
     rows = []
-    column_headers = ('Rule', 'Endpoint', 'Arguments')
+    column_headers = ("Rule", "Endpoint", "Arguments")
 
     if url:
         try:
-            rule, arguments = (
-                current_app.url_map.bind('localhost')
-                .match(url, return_rule=True))
+            rule, arguments = current_app.url_map.bind("localhost").match(
+                url, return_rule=True
+            )
             rows.append((rule.rule, rule.endpoint, arguments))
             column_length = 3
         except (NotFound, MethodNotAllowed) as e:
-            rows.append(('<{}>'.format(e), None, None))
+            rows.append(("<{}>".format(e), None, None))
             column_length = 1
     else:
         rules = sorted(
             current_app.url_map.iter_rules(),
-            key=lambda rule: getattr(rule, order))
+            key=lambda rule: getattr(rule, order),
+        )
         for rule in rules:
             rows.append((rule.rule, rule.endpoint, None))
         column_length = 2
 
-    str_template = ''
+    str_template = ""
     table_width = 0
 
     if column_length >= 1:
         max_rule_length = max(len(r[0]) for r in rows)
         max_rule_length = max_rule_length if max_rule_length > 4 else 4
-        str_template += '{:' + str(max_rule_length) + '}'
+        str_template += "{:" + str(max_rule_length) + "}"
         table_width += max_rule_length
 
     if column_length >= 2:
         max_endpoint_length = max(len(str(r[1])) for r in rows)
         max_endpoint_length = (
-            max_endpoint_length if max_endpoint_length > 8 else 8)
-        str_template += '  {:' + str(max_endpoint_length) + '}'
+            max_endpoint_length if max_endpoint_length > 8 else 8
+        )
+        str_template += "  {:" + str(max_endpoint_length) + "}"
         table_width += 2 + max_endpoint_length
 
     if column_length >= 3:
         max_arguments_length = max(len(str(r[2])) for r in rows)
         max_arguments_length = (
-            max_arguments_length if max_arguments_length > 9 else 9)
-        str_template += '  {:' + str(max_arguments_length) + '}'
+            max_arguments_length if max_arguments_length > 9 else 9
+        )
+        str_template += "  {:" + str(max_arguments_length) + "}"
         table_width += 2 + max_arguments_length
 
     click.echo(str_template.format(*column_headers[:column_length]))
-    click.echo('-' * table_width)
+    click.echo("-" * table_width)
 
     for row in rows:
         click.echo(str_template.format(*row[:column_length]))
@@ -145,5 +164,7 @@ def urls(url, order):
 
 @click.command()
 def deploy():
-    # migrate database to latest revision
+    """
+    migrate database to latest revision
+    """
     upgrade()
