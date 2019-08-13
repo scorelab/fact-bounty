2e582f3e923f4b74becc793ca203290b41a8aca3
anmolbansal1 <abansal@ec.iitr.ac.in>
2019-08-13 23:44:13 +0530

    Change data type of date column from string to date
    Create new script to create index with specified data type of columns.
    Change  format of news in crawlers

--- Crawlers/news_sites/pipelines.py
+++ Crawlers/news_sites/pipelines.py
@@ -4,6 +4,7 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import dateutil.parser as dparser
 
 
 class NewsSitesPipeline(object):
@@ -11,5 +12,7 @@ class NewsSitesPipeline(object):
         item["approved_count"] = 0
         item["fake_count"] = 0
         item["mixedvote_count"] = 0
+        date = dparser.parse(item["date"]).date()
+        item["date"] = date.isoformat()
 
         return item
