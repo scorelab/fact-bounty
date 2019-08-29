f8a2b6e63be824e66fc118c05c182bacc1edb881
root5533 <ymuraad@gmail.com>
2019-08-29 20:37:09 +0530

    Display recent tweets in landing page

---
+++ fact-bounty-client/src/redux/actions/twitterActions.js
@@ -0,0 +1,33 @@
+import { FETCH_TWEETS, LOADING_TWEETS } from './actionTypes'
+import Twitter from '../../helpers/twitter'
+
+export const fetchTweets = (limit, user) => dispatch => {
+  dispatch({
+    type: LOADING_TWEETS
+  })
+  const twitter = Twitter()
+  twitter
+    .getUserTweets(limit, user)
+    .then(tweets => {
+      let tweetItems = []
+      tweets.map(tweet => {
+        const item = {
+          content: tweet.text,
+          date: tweet.created_at,
+          title: tweet.user.name,
+          _id: tweet.id_str,
+          approved_count: 0,
+          mixedvote_count: 0,
+          fake_count: 0
+        }
+        tweetItems.push(item)
+      })
+      dispatch({
+        type: FETCH_TWEETS,
+        payload: tweetItems
+      })
+    })
+    .catch(err => {
+      console.error('Invalid response:', err)
+    })
+}
