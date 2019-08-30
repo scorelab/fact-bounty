71be8b18bffb38618777721df7d0497b41e4925d
Devon Wijesinghe <devon@Devons-MacBook-Pro.local>
2019-08-30 21:03:12 +0530

    Quick fix for server side routes issue

--- fact-bounty-flask/app.py
+++ fact-bounty-flask/app.py
@@ -12,6 +12,7 @@ if os.path.exists(dotenv_path):
 app = create_app(os.getenv("FLASK_CONFIG") or "default")
 
 
-@app.route("/")
-def default_route():
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def default_route(path):
     return render_template("index.html")
