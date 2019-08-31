b22a7b65ec4799174daa13562f118f9085ed55c0
Devon Wijesinghe <devon@Devons-MacBook-Pro.local>
2019-08-31 17:54:53 +0530

    Minnor changes

--- fact-bounty-flask/app.py
+++ fact-bounty-flask/app.py
@@ -16,3 +16,6 @@ app = create_app(os.getenv("FLASK_CONFIG") or "default")
 @app.route('/<path:path>')
 def default_route(path):
     return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
