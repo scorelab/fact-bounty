20397e4d7fc9fa246807fb32275e971d3e083ae2
Anmol Bansal <39100247+Anmolbansal1@users.noreply.github.com>
2019-12-13 10:48:49 +0530

    Update stories test
    Fix test class title
    Add new test for single story

--- fact-bounty-flask/tests/test_story.py
+++ fact-bounty-flask/tests/test_story.py
@@ -3,37 +3,45 @@ import unittest
 import tempfile
 import json
 
-FLASKR = __import__('fact-bounty-flask')
+FLASKR = __import__("fact-bounty-flask")
 
 
-class Test_Login(unittest.TestCase):
+class Test_Story(unittest.TestCase):
     def setUp(self):
         self.db_fd,
-        FLASKR.config['SQLALCHEMY_DATABASE_URI'] = tempfile.mkstemp()
+        FLASKR.config["SQLALCHEMY_DATABASE_URI"] = tempfile.mkstemp()
         FLASKR.testing = True
         self.app = FLASKR.app.app.test_client()
 
     def tearDown(self):
         os.close(self.db_fd)
-        os.unlink(FLASKR.config['SQLALCHEMY_DATABASE_URI'])
+        os.unlink(FLASKR.config["SQLALCHEMY_DATABASE_URI"])
 
     def test_fetch_all_stories_200(self):
         """Fetch all stories"""
-        response = self.app.get('/api/stories/all')
-        res = response.data.decode('ASCII')
+        response = self.app.get("/api/stories/all")
+        res = response.data.decode("ASCII")
         res = json.loads(res)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(res['message'], 'Stories successfully fetched')
-        self.assertTrue(isinstance(res['stories'], (list)))
+        self.assertEqual(res["message"], "Stories successfully fetched")
+        self.assertTrue(isinstance(res["stories"], (list)))
 
     def test_fetch_range_of_stories_200(self):
         """Fetch range of stories"""
-        response = self.app.get('/api/stories/get-range/1')
-        res = response.data.decode('ASCII')
+        response = self.app.get("/api/stories/get-range/1")
+        res = response.data.decode("ASCII")
         res = json.loads(res)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(res['message'], 'Stories successfully fetched')
-        self.assertTrue(isinstance(res['stories'], (list)))
+        self.assertEqual(res["message"], "Stories successfully fetched")
+        self.assertTrue(isinstance(res["stories"], (list)))
+
+    def test_fetch_single_story(self):
+        """Fetch a single story"""
+        response = self.app.get("/api/stories/get/1")
+        res = response.data.decode("ASCII")
+        res = json.loads(res)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(res["message"], "Story fetched successfully")
 
 
 if __name__ == "__main__":
