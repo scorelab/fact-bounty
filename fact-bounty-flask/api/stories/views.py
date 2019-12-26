dc679846c0ea7cbb5007a7342e145ad5b2eefdbb
anmolbansal1 <abansal@ec.iitr.ac.in>
2019-12-26 14:17:12 +0530

    Fix general convention of route in flask app Fix route error in admin blueprint Fix syntax error in app.py

--- fact-bounty-flask/api/stories/views.py
+++ fact-bounty-flask/api/stories/views.py
@@ -5,35 +5,33 @@ storyprint = Blueprint("stories", __name__)
 
 
 storyprint.add_url_rule(
-    "/stories/all", view_func=storyController["allstories"], methods=["GET"]
+    "/all", view_func=storyController["allstories"], methods=["GET"]
 )
 
 storyprint.add_url_rule(
-    "/stories/get-range/<int:page>",
+    "/get-range/<int:page>",
     view_func=storyController["getrange"],
     methods=["GET"],
 )
 
 storyprint.add_url_rule(
-    "/stories/get/<string:id>",
-    view_func=storyController["getstory"],
-    methods=["GET"],
+    "/get/<string:id>", view_func=storyController["getstory"], methods=["GET"]
 )
 
 storyprint.add_url_rule(
-    "/stories/search/<string:keyword>",
+    "/search/<string:keyword>",
     view_func=storyController["search"],
     methods=["GET"],
 )
 
 storyprint.add_url_rule(
-    "/stories/load-user-votes",
+    "/load-user-votes",
     view_func=storyController["loaduservotes"],
     methods=["POST"],
 )
 
 storyprint.add_url_rule(
-    "/stories/change-vote-count",
+    "/change-vote-count",
     view_func=storyController["changevote"],
     methods=["POST"],
 )
