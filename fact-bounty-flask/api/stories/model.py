f2410bf4801720d06812f54649b9109449c526e9
Joseph Semrai <josephsemrai@gmail.com>
2020-01-05 20:49:10 -0500

    Add backend comment support
    Add delete
    
    Change content
    
    Change delete
    
    Codacy fixes
    
    Codacy fix
    
    Codacy fix
    
    Rename class
    
    Add decorator

--- fact-bounty-flask/api/stories/model.py
+++ fact-bounty-flask/api/stories/model.py
@@ -30,3 +30,35 @@ class Vote(Model):
         """
         db.session.add(self)
         db.session.commit()
+
+class Comment(Model):
+    """
+    This model represents a comment on a given note
+    """
+
+    __tablename__ = "comment"
+
+    id = Column(db.Integer, primary_key=True)
+    story_id = Column(db.String(128))
+    user_id = Column(db.Integer, db.ForeignKey("user.id"))
+    content = Column(db.Integer)
+
+    def __init__(self, story_id, user_id, content):
+        self.story_id = story_id
+        self.user_id = user_id
+        self.content = content
+
+    @classmethod
+    def fetch_user_comments(cls, user_id):
+        return cls.query.filter_by(user_id=user_id).all()
+
+    @classmethod
+    def fetch_post_comments(cls, post_id):
+        return cls.query.filter_by(post_id=post_id).all()
+
+    def save(self):
+        """
+        Save a comment to the database.
+        """
+        db.session.add(self)
+        db.session.commit()
