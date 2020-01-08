829b0e83d083d50934f7c9b15e2bc437b5b62b69
sibukumar98 <sibukumar98@gmail.com>
2020-01-08 18:23:14 +0530

    Change in Login UI

--- fact-bounty-client/src/pages/Login/Login.jsx
+++ fact-bounty-client/src/pages/Login/Login.jsx
@@ -251,7 +251,7 @@ class Login extends Component {
             <OauthContainer button_type="Login" />
           </form>
           <p>
-            Don&apost have an account?&nbsp
+            Don't have an account?
             <Link component={RouterLink} to="/register">
               Create One.
             </Link>
