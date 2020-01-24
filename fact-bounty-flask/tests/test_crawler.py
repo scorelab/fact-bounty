ae465c179376b73e40ad6fe58b9e20be82fdfd35
hv7214 <hverma1@cs.iitr.ac.in>
2020-01-24 19:49:01 +0530

    fix(server/tests): appended root folder system path

--- fact-bounty-flask/tests/test_crawler.py
+++ fact-bounty-flask/tests/test_crawler.py
@@ -2,34 +2,40 @@ import os
 import unittest
 import tempfile
 import json
+import sys
 
-FLASKR = __import__('fact-bounty-flask')
+sys.path.append(os.path.join(sys.path[0], "../../"))
+FLASKR = __import__("fact-bounty-flask")
 
 
 class Test_Crawler(unittest.TestCase):
     def setUp(self):
-        FLASKR.config['SQLALCHEMY_DATABASE_URI'] = tempfile.mkstemp()
+        FLASKR.config["SQLALCHEMY_DATABASE_URI"] = tempfile.mkstemp()
         FLASKR.testing = True
         self.app = FLASKR.app.app.test_client()
 
     def tearDown(self):
         os.close(self.db_fd)
-        os.unlink(FLASKR.config['SQLALCHEMY_DATABASE_URI'])
+        os.unlink(FLASKR.config["SQLALCHEMY_DATABASE_URI"])
 
     def test_fetch_all_jobs_200(self):
         """Fetch all jobs"""
-        response = self.app.get('/api/crawler/cron_job')
-        res = response.data.decode('ASCII')
+        response = self.app.get("/api/crawler/cron_job")
+        res = response.data.decode("ASCII")
         res = json.loads(res)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(res['message'], 'Jobs fetched successfully')
-        self.assertTrue(isinstance(res['jobs'], (list)))
+        self.assertEqual(res["message"], "Jobs fetched successfully")
+        self.assertTrue(isinstance(res["jobs"], (list)))
 
     def test_fetch_crawl_200(self):
         """Fetch all tasks and start crawling"""
-        response = self.app.get('/api/crawler/crawl_live')
-        res = response.data.decode('ASCII')
+        response = self.app.get("/api/crawler/crawl_live")
+        res = response.data.decode("ASCII")
         res = json.loads(res)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(res['message'], 'Started Crawling today news')
-        self.assertTrue(isinstance(res['tasks'], (list)))
+        self.assertEqual(res["message"], "Started Crawling today news")
+        self.assertTrue(isinstance(res["tasks"], (list)))
+
+
+if __name__ == "__main__":
+    unittest.main()
--- fact-bounty-flask/tests/test_register.py
+++ fact-bounty-flask/tests/test_register.py
@@ -2,82 +2,93 @@ import os
 import unittest
 import tempfile
 import json
+import sys
 
-FLASKR = __import__('fact-bounty-flask')
+sys.path.append(os.path.join(sys.path[0], "../../"))
+FLASKR = __import__("fact-bounty-flask")
 
 USER_DATA = dict(
-    name='name',
-    email='example@gmail.com',
-    password='password',
-    password2='password'
+    name="name",
+    email="example@gmail.com",
+    password="password",
+    password2="password",
 )
 
 
 class Test_Register(unittest.TestCase):
     def setUp(self):
         self.db_fd,
-        FLASKR.config['SQLALCHEMY_DATABASE_URI'] = tempfile.mkstemp()
+        FLASKR.config["SQLALCHEMY_DATABASE_URI"] = tempfile.mkstemp()
         FLASKR.testing = True
         self.app = FLASKR.app.app.test_client()
 
     def tearDown(self):
         os.close(self.db_fd)
-        os.unlink(FLASKR.config['SQLALCHEMY_DATABASE_URI'])
+        os.unlink(FLASKR.config["SQLALCHEMY_DATABASE_URI"])
         # FLASKR.db.session.remove()
         # FLASKR.db.drop_all()
 
     def test_can_register(self):
         """Register a new user."""
         response = self.app.post(
-            '/api/users/register',
+            "/api/users/register",
             data=json.dumps(
                 dict(
-                    name=USER_DATA['name'],
-                    email=USER_DATA['email'],
-                    password=USER_DATA['password'],
-                    password2=USER_DATA['password2'])),
-            content_type='application/json',
-            follow_redirects=True)
-        res = response.data.decode('ASCII')
+                    name=USER_DATA["name"],
+                    email=USER_DATA["email"],
+                    password=USER_DATA["password"],
+                    password2=USER_DATA["password2"],
+                )
+            ),
+            content_type="application/json",
+            follow_redirects=True,
+        )
+        res = response.data.decode("ASCII")
         res = json.loads(res)
         self.assertEqual(response.status_code, 202)
         self.assertEqual(
-            res['message'],
-            'You registered successfully. Please log in.')
+            res["message"], "You registered successfully. Please log in."
+        )
 
     def test_sees_error_message_if_passwords_dont_match(self):
         """Show error if passwords don't match."""
         response = self.app.post(
-            '/api/users/register',
+            "/api/users/register",
             data=json.dumps(
                 dict(
-                    name=USER_DATA['name'],
-                    email=USER_DATA['email'] + 'x',
-                    password=USER_DATA['password'],
-                    password2=USER_DATA['password2'] + 'x')),
-            content_type='application/json',
-            follow_redirects=True)
-        res = response.data.decode('ASCII')
+                    name=USER_DATA["name"],
+                    email=USER_DATA["email"] + "x",
+                    password=USER_DATA["password"],
+                    password2=USER_DATA["password2"] + "x",
+                )
+            ),
+            content_type="application/json",
+            follow_redirects=True,
+        )
+        res = response.data.decode("ASCII")
         res = json.loads(res)
         self.assertEqual(response.status_code, 401)
-        self.assertEqual(res['message'], 'Both passwords does not match')
+        self.assertEqual(res["message"], "Both passwords does not match")
 
     def test_sees_error_message_if_user_already_registered(self):
         """Show error if user already registered."""
         response = self.app.post(
-            '/api/users/register',
+            "/api/users/register",
             data=json.dumps(
                 dict(
-                    name=USER_DATA['name'],
-                    email=USER_DATA['email'],
-                    password=USER_DATA['password'],
-                    password2=USER_DATA['password2'])),
-            content_type='application/json',
-            follow_redirects=True)
-        res = response.data.decode('ASCII')
+                    name=USER_DATA["name"],
+                    email=USER_DATA["email"],
+                    password=USER_DATA["password"],
+                    password2=USER_DATA["password2"],
+                )
+            ),
+            content_type="application/json",
+            follow_redirects=True,
+        )
+        res = response.data.decode("ASCII")
         res = json.loads(res)
         self.assertEqual(response.status_code, 202)
-        self.assertEqual(res['message'], 'User already exists. Please login.')
+        self.assertEqual(res["message"], "User already exists. Please login.")
 
 
 if __name__ == "__main__":
