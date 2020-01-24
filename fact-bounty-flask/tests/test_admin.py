73d1b6e3f831198ed7b123cb580e9047c3d63625
hv7214 <hverma1@cs.iitr.ac.in>
2020-01-24 19:49:01 +0530

    fix(server/tests): appended root folder system path

--- fact-bounty-flask/tests/test_admin.py
+++ fact-bounty-flask/tests/test_admin.py
@@ -2,25 +2,31 @@ import os
 import unittest
 import tempfile
 import json
+import sys
 
-FLASKR = __import__('fact-bounty-flask')
+sys.path.append(os.path.join(sys.path[0], "../../"))
+FLASKR = __import__("fact-bounty-flask")
 
 
 class Test_Admin(unittest.TestCase):
     def setUp(self):
-        FLASKR.config['SQLALCHEMY_DATABASE_URI'] = tempfile.mkstemp()
+        FLASKR.config["SQLALCHEMY_DATABASE_URI"] = tempfile.mkstemp()
         FLASKR.testing = True
         self.app = FLASKR.app.app.test_client()
 
     def tearDown(self):
         os.close(self.db_fd)
-        os.unlink(FLASKR.config['SQLALCHEMY_DATABASE_URI'])
+        os.unlink(FLASKR.config["SQLALCHEMY_DATABASE_URI"])
 
     def test_fetch_system_panel_200(self):
         """Get System Panel Information"""
-        response = self.app.get('/api/admin/system')
-        res = response.data.decode('ASCII')
+        response = self.app.get("/api/admin/system")
+        res = response.data.decode("ASCII")
         res = json.loads(res)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(res['message'], 'Data fetched successfully!')
-        self.assertTrue(isinstance(res['data'], (dict)))
+        self.assertEqual(res["message"], "Data fetched successfully!")
+        self.assertTrue(isinstance(res["data"], (dict)))
+
+
+if __name__ == "__main__":
+    unittest.main()
